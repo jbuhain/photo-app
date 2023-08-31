f047d7271601ffe7c32efbef502ba59bf66233e8
Joshua Buhain <jbuhain1@gmail.com>
2023-08-31 01:05:21 -0700

    transferred from local

---
+++ src/main/java/com/example/josh/photoz/clone/repository/PhotozRepository.java
@@ -0,0 +1,8 @@
+package com.example.josh.photoz.clone.repository;
+
+import com.example.josh.photoz.clone.model.Photo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhotozRepository extends CrudRepository<Photo, Integer> {
+
+}
---
+++ src/main/java/com/example/josh/photoz/clone/service/PhotozService.java
@@ -0,0 +1,41 @@
+package com.example.josh.photoz.clone.service;
+
+import com.example.josh.photoz.clone.model.Photo;
+import com.example.josh.photoz.clone.repository.PhotozRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collection;
+import java.util.UUID;
+
+@Service
+//@Component
+public class PhotozService {
+    private final PhotozRepository photozRepository;
+
+    public PhotozService(PhotozRepository photozRepository) {
+        this.photozRepository = photozRepository;
+    }
+
+    public Iterable<Photo> get() {
+        return photozRepository.findAll();
+    }
+
+    public Photo get(Integer id) {
+        return photozRepository.findById(id).orElse(null);
+    }
+
+    public void remove(Integer id) {
+        photozRepository.deleteById(id);
+    }
+
+    public Photo save(String fileName, String contentType, byte[] data) {
+        Photo photo = new Photo();
+        photo.setContentType(contentType);
+        photo.setFileName(fileName);
+        photo.setData(data);
+        photozRepository.save(photo);
+        return photo;
+    }
+}
